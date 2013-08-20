3fac0a9bcfc3a63b74dbeef0b443dbfad14b96b6
Benjamin Vialle <benjamin@vialle.io>
2013-08-20 16:43:49 +0200

    First commit with ruby user install

---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: markus
+# Recipe:: default
+#
+# Copyright 2013, MarkUsProject
+#
+
+# installing ruby for user markus
+ruby_build_ruby "#{node[:markus][:ruby_version]" do
+  prefix_path "#{node[:markus][:ruby_path]}/#{node[:markus][:ruby_version]"
+  user        "markus"
+  group       "markus"
+  action      :install
+end
---
+++ recipes/production.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: markus
+# Recipe:: source
+#
+# Copyright 2013, MarkUsProject
+#
+
+include_recipe "markus::default"
+
