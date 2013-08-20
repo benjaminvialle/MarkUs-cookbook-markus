04d15aad0bb3e858aa83f9c2b4b98c4e1a939acb
Benjamin Vialle <benjamin@vialle.io>
2013-08-20 16:45:29 +0200

    Fix: typo

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@
 #
 
 # installing ruby for user markus
-ruby_build_ruby "#{node[:markus][:ruby_version]" do
+ruby_build_ruby "#{node[:markus][:ruby_version]}" do
   prefix_path "#{node[:markus][:ruby_path]}/#{node[:markus][:ruby_version]"
   user        "markus"
   group       "markus"
